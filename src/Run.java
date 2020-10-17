97a1354dd45b05ce5ceb8b2a0d2ac6c3fb8bbdb2
ygarabimoghadam <ygarabimoghadam@gmail.com>
2020-10-18 00:38:48 +0330

    done

---
+++ src/Run.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class Run {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        int second =  now.getSecond();
+        int minute = now.getMinute();
+        int hour = now.getHour();
+        ClockDisplay myClock = new ClockDisplay(hour,minute,second);
+        myClock.timeTick();
+        myClock.print();
+        myClock.setTime(1,59,59);
+        myClock.timeTick();
+        myClock.print();
+
+    }
+}
